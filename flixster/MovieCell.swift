6b02fcefa8b44f659929654a2592038211d48dfa
Luis Ramos <lr2471311@gmail.com>
2020-01-29 22:15:12 -0800

    Initial Commit

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Luis Ramos on 1/23/20.
+//  Copyright © 2020 SMC Software. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Luis Ramos on 1/23/20.
+//  Copyright © 2020 SMC Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
